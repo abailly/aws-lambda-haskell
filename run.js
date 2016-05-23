5647b67572b53c771a066f4cf8e7cbc77d59f5af
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-05-23 09:57:02 +0200

    added js runner

---
+++ run.js
@@ -0,0 +1,18 @@
+const spawn = require('child_process').spawn;
+const main = spawn('./main');
+
+main.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+main.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+main.on('error', (err) => {
+  console.log(`error: ${err}`);
+});
+
+main.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
