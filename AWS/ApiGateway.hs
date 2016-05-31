2fb5d3be2e9ae08640cf2f32241fe00d2e670a28
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-05-31 18:15:18 +0200

    use system-extra to run docker build

--- AWS/ApiGateway.hs
+++ AWS/ApiGateway.hs
@@ -2,12 +2,8 @@ module AWS.ApiGateway where
 
 import Control.Monad.Catch
 import           Control.Lens
-import           Control.Monad.IO.Class
 import           Control.Monad.Trans.AWS
-import           Data.Monoid
-import           Network.AWS.Data
 import           Network.AWS.APIGateway
-import           System.IO
 import Control.Monad.Trans.Resource
 import Data.Text(Text)
 
@@ -17,7 +13,7 @@ createApi api = do
     let Just apiId = restApi ^. raId
     resources :: [Resource] <- (view grrsItems) <$> send (getResources apiId)
     let Just rootId = head resources ^. rId
-    resource :: Resource <- send (createResource apiId rootId "{file}")
-    let Just fileResourceId = resource ^. rId
-    method :: Method <- send (putMethod apiId fileResourceId "GET" "NONE")
-    return method
+    resrc :: Resource <- send (createResource apiId rootId "{file}")
+    let Just fileResourceId = resrc ^. rId
+    meth :: Method <- send (putMethod apiId fileResourceId "GET" "NONE")
+    return meth
--- main.hs
+++ main.hs
@@ -7,8 +7,10 @@ import           Control.Monad.Trans.AWS
 import           Data.Text               (pack)
 import           Prelude                 hiding (null)
 import           System.Build
+import           System.Docker
 import           System.Environment
 import           System.IO
+import           System.Process          (callProcess)
 
 main :: IO ()
 main = do
@@ -16,7 +18,7 @@ main = do
   -- build docker container
   buildDocker
   -- build executable with docker
-  exe <- buildExecutable sourceDirectory targetName
+  exe <- stackInDocker (ImageName "ghc-centos") sourceDirectory targetName
   -- pack executable with js shim in .zip file
   _ <- packLambda exe
   lgr <- newLogger Trace stdout
@@ -27,4 +29,8 @@ main = do
 
       createApiEndpoint env api = runResourceT (runAWST env $ createApi $ pack api)
 
+      buildDocker :: IO ()
+      buildDocker = callProcess "docker" ["build", "-t", "ghc-centos","ghc-centos" ]
 
+      packLambda :: FilePath -> IO ()
+      packLambda exe = callProcess "zip" [ "lambda.zip", "run.js", exe ]
